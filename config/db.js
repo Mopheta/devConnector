d1a6f24373cd2d0c1f2a31a2cb68508902afd16b
Mopheta <kevinstalker14@gmail.com>
2020-04-14 19:36:26 -0300

    Connecting to MongoDB with mongoose

---
+++ config/db.js
@@ -0,0 +1,21 @@
+//MONGO DB CONNECTION
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI'); //lo trae de default file
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true, //Si no te da un error de consola debido a un strin deprecated
+    });
+
+    console.log('Kevin -- MongoDB connected... yeah');
+  } catch (err) {
+    console.error(err.message);
+
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+//Connect Database
+connectDB();
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('API Running')); //para testear con postman
+
+const PORT = process.env.PORT || 5000; //busca si hay un puerto(luego heroku si no abre el 5000)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
