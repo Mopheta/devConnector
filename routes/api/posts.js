126c9224df7c5a493cb6762660eba77db8cf5fd7
Mopheta <kevinstalker14@gmail.com>
2020-04-14 21:11:19 -0300

    Routes files with express router

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+//routes
+const express = require('express');
+const router = express.Router();
+
+//@route  GET api/posts
+//@desc   Test route
+//@access Public
+router.get('/', (req, res) => res.send('Posts route'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running')); //para testear con postman
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000; //busca si hay un puerto(luego heroku si no abre el 5000)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
