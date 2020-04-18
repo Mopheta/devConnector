d71f207d0044b2894e2b2b7341e4f634a38dbc89
Mopheta <kevinstalker14@gmail.com>
2020-04-18 18:15:07 -0300

    Get byId, get all and delete posts routes created.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,4 +44,66 @@ router.post(
   }
 );
 
+//@route  GET api/posts
+//@desc   GET all post
+//@access Private - tenes que estar logeado
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); //newest first
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route  GET api/posts/:id
+//@desc   GET post by id
+//@access Private - tenes que estar logeado
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id); //newest first
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      //Objeto err.propiedad kind, es igual a objectID, si lo es,
+      //No es un formated object ID
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route  DELETE api/posts/:id
+//@desc   DELETE post by id
+//@access Private - tenes que estar logeado
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check on the user (only user owener of the post can delete)
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
